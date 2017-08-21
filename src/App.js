452add7b9e4ce347a493771593539d90788d66c2
Alexandre Martinez <alexandre.martinez76@gmail.com>
2017-08-21 22:50:49 +0200

    Use Bootstrap for layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import {Grid, Row, Col} from 'react-bootstrap';
 import Commands from './Commands';
 import Details from './Details';
 import Main from './Main';
 
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Commands />
-        <Main />
-        <Details />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Grid className="App container">
+                <Row>
+                    <Col md={2}><Commands/></Col>
+                    <Col md={8}><Main/></Col>
+                    <Col md={2}><Details/></Col>
+                </Row>
+            </Grid>
+        );
+    }
 }
 
 export default App;
